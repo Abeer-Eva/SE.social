d521a2715f08d5e4b5a846777d10f1fdcbcccf17
Sarahmaliin <sarahmalinkarlsson@gmail.com>
2022-02-09 13:20:01 +0100

    sarahs första push

--- src/components/Login.js
+++ src/components/Login.js
@@ -5,6 +5,7 @@ const Login =() =>{
         <div id="login-page">
             <div id="login-card">
                 <h2> Welcome</h2>
+                <p>sarah testar</p>
             </div>
         </div>
     );
