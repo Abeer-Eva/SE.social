ab7c8f5ede1fe010ca7f93ead15bd93788a1ab8f
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2022-02-09 11:15:19 +0100

    first

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Switch, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+
+function App() {
+  return (
+    <div className="App">
+       <Router>
+        {/* <AuthProvider> */}
+          <Routes>
+            {/* <Route path="/chats" component={Chats} />*/}
+            <Route path="/" component={Login} /> 
+          </Routes> 
+        {/* </AuthProvider> */}
+      </Router>
+    </div>
+  );
+}
+
+export default App;
