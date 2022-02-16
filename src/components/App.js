2509f0c3554178f224574bf136f99bf5357328a6
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2022-02-16 09:15:16 +0100

    firebase

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Login";
 
 function App() {
+
   return (
     <div style={{ fontFamily: 'Avenir' }}>
        <Router>
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,41 @@
 import React from 'react';
 import { GoogleOutlined, FacebookOutlined} from '@ant-design/icons'
 import 'firebase/app';
-import { auth, provider } from './firebase';
-import { signInWithRedirect } from "firebase/auth";
+import { auth, provider } from "./firebase";
+import { signInWithPopup, signInWithRedirect,getAuth } from "firebase/auth";
 
 
 
-// // Sign in using a redirect.
 
-// // Start a sign in process for an unauthenticated user.
-// provider.addScope('profile');
-// provider.addScope('email');
-// await signInWithRedirect(auth, provider);
-const Login =() =>{
 
+
+// Sign in using a redirect.
+const Login = () =>{
+  
+
+    const signIn = () => {
+        signInWithPopup(provider)
+         }
+console.log(provider);
     return (
         <div id="login-page ">
             <div id="login-card">
                 <h2> Welcome</h2>
 
-                <div className='login-button  google' onClick={()=> signInWithRedirect(new provider)}>
+                <div className='login-button  google' onClick={signIn} >
+                
                     <GoogleOutlined/> SIgn in with google
                 </div>
                 <br/>
                 <br/>
-                <div className='login-button  facebook' onClick={()=> signInWithRedirect(new provider)}>
+                <div className='login-button  facebook' onClick={signIn}>
                     <FacebookOutlined/> SIgn in with facebook
                 </div>
 
             </div>
         </div>
     );
+   
 }
 
 export default Login
