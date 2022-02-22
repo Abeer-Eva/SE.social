94a4bd05d266844a857113ec2f6927cef3dc202a
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2022-02-22 16:58:25 +0100

    ..

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
+import moment from 'moment'
 import '../style/Post.css'
+import { doc, updateDoc } from "firebase/firestore";
+import {db} from '../components/firebase'
 
-const Post = ({ anv, profilePic, message, timestamp, username, image, like}) => {
-    //timestamp importeras inte korrekt här
+const Post = ({ anv, profilePic, message, timeStamp, username, image, like}) => {
     
-const Post = ({ anv, message, timeStamp, username, image}) => {
-  
-  const handleClick= async (like) =>{
-          await (like++)
-
-      }
+   const updateLike = async (post) => {
+      const likeRef = doc(db, "postData", anv);
+      await updateDoc(likeRef, {
+          like: like+1
+      });
+  };
+  updateLike(like);
   
     //timestamp importeras inte korrekt här
     const time = new Date(timeStamp*1000)
@@ -20,22 +23,20 @@ const Post = ({ anv, message, timeStamp, username, image}) => {
   return (
     <section className='post'>
        
-    
             <section className='post_topinfo'>
-                <img  className='prof' src={profilePic}/>
+            <img alt='profile pic' className='prof' src={profilePic}/>
                 <h3 className='userName'>{username}</h3>
-                <p className='timestamp'>{timeS}</p>
+                <p className='timestamp'>{date}</p>
             </section>
         
-
         <section className='post_bottom'>
             <p className='message'>{message}</p>
             <img src={image} alt="" className='post_image'/>
-             <div className='like' onClick={handleClick}>   <ThumbUpIcon /> {like}</div>  
+             <div className='like' onClick={updateLike(like)}>   <ThumbUpIcon /> {like}</div>  
             </section>
         
-
     </section>
   )
 }
+
 export default Post
