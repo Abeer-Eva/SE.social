10b29af7b39d4eafaa869da93c88b6179fb588b1
Sarahmalin <sarahmalinkarlsson@gmail.com>
2022-02-22 16:34:25 +0100

    fixade timestamp för inläggen på postsidan

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,44 +1,35 @@
 import React from 'react'
 import ThumbUpIcon from '@material-ui/icons/ThumbUp';
-import {auth} from '../components/firebase'
+import moment from 'moment'
 import '../style/Post.css'
 
-const Post = ({ anv, profilePic, message, timestamp, username, image, like}) => {
-    //timestamp importeras inte korrekt här
+const Post = ({ anv, profilePic, message, timeStamp, username, image, like}) => {
     
-const Post = ({ anv, message, timeStamp, username, image}) => {
-  
   const handleClick= async (like) =>{
-          await (like++)
+      await (like++)
+    }
 
-      }
-  
-    //timestamp importeras inte korrekt här
-    const time = new Date(timeStamp*1000)
-    const timeS = time.toString()
-<<<<<<< HEAD
-=======
-    console.log(time)
-  
->>>>>>> 33b0a35c40d2b3a9590a98bd42d8bbd02625d933
+    const timeStampDate = timeStamp 
+    const dateInMills = timeStampDate * 1000
+    const day = new Date(dateInMills).toDateString()
+
+    let date = moment(day).format('ddd, MMM Do, hA')
+
   return (
     <section className='post'>
        
-    
             <section className='post_topinfo'>
-                <img  className='prof' src={profilePic}/>
+            <img alt='profile pic' className='prof' src={profilePic}/>
                 <h3 className='userName'>{username}</h3>
-                <p className='timestamp'>{timeS}</p>
+                <p className='timestamp'>{date}</p>
             </section>
         
-
         <section className='post_bottom'>
             <p className='message'>{message}</p>
             <img src={image} alt="" className='post_image'/>
              <div className='like' onClick={handleClick}>   <ThumbUpIcon /> {like}</div>  
             </section>
         
-
     </section>
   )
 }
