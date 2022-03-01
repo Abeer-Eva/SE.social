a10daff7eb0095b3020cc80865a3bdbb4d84fd61
Sarahmalin <sarahmalinkarlsson@gmail.com>
2022-03-01 13:52:51 +0100

    fixat getUser så anv skrivs ut bara en gång.

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import GetUser from './GetUser'
 import {Link} from 'react-router-dom'
 import BottomNavigation from '@mui/material/BottomNavigation';
 import BottomNavigationAction from '@mui/material/BottomNavigationAction';
--- src/components/getUser.jsx
+++ src/components/getUser.jsx
@@ -7,35 +7,41 @@ import { collection, getDocs } from "firebase/firestore";
 
 
 
-
 // Sign out anv av en redirect.
 const GetUsers = () =>{
     const [list ,SetList]= useState([])
+    const names = []
     //  const [state, dispatch] = useStateValue() //låter oss ta information från datan
 
         const getAllUsers = async () => {
         const usersSnapshot = await getDocs(collection(db, "postData"));
-
-        
         const usersList = usersSnapshot.docs.map((doc) => doc.data());
-     
         SetList(usersList);
         return usersList;
-    
-        
     };  
+    getAllUsers();
+    
         // dispatch ({
         //     type: actionTypes.GET_USRES,
         //     user: '', //pushar svaret in till actiontypes användardata
         // })
-        getAllUsers();
-    
+        
+
+        const nameIt = list.map(( name =>
+                {if(!names.includes(name.userName)) {
+                  names.push(name.userName)  
+                }
+                else{
+                    names.splice(names.indexOf(name.userName), 1)
+                }}
+                ))
+            
     return (
         <div  >
             <div className='UsersList'>
-              {list.map(users=>(
+              {names.map(users =>(
                      <section key={users.id} className="user-card">
-                     <h3>{users.userName}</h3>
+                     <h3>{users}</h3>
                     
                 </section>))
               }
