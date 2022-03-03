987dd64cdb590202e0ee86334cbe721fb075409e
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2022-03-03 11:45:24 +0100

    start chat sida

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,7 +1,12 @@
 import { LoginOutlined, SmileFilled } from '@ant-design/icons'
 import { signOut } from 'firebase/auth'
 import React, { useState } from 'react'
-import { ChatEngine, getOrCreateChat } from 'react-chat-engine'
+import { 
+    ChatEngine, 
+    ChatList, ChatCard, NewChatForm,
+    ChatFeed, ChatHeader, IceBreaker, MessageBubble, IsTyping, ConnectionBar, NewMessageForm,
+    ChatSettings, ChatSettingsTop, PeopleSettings, PhotosSettings, OptionsSettings,getOrCreateChat
+} from 'react-chat-engine'
 import axios from 'axios'
 import { AuthProvider } from '../context/AuthContext'
 
@@ -9,31 +14,32 @@ import { AuthProvider } from '../context/AuthContext'
 const DirectChatPage = () => {
 	const [username, setUsername] = useState('')
 	
-	function createDirectChat(creds) {
-		getOrCreateChat(
-			creds,
-			{ is_direct_chat: true, usernames: [username] },
-			() => setUsername('')
-		)
-	}
+	// function createDirectChat(creds) {
+	// 	getOrCreateChat(
+	// 		creds,
+	// 		{ is_direct_chat: true, usernames: [username] },
+	// 		() => setUsername('')
+	// 	)
+	// }
     
-	function getOrCreateUser(callback) {
-        axios.put(
-            'https://api.chatengine.io/users/',
-            {username: [username]},
-            {headers: {"Private-Key": process.env.projectID}}
-        )
-        .then(r => callback(r.data))
-        .catch(e => console.log('Get or create user error', e))
+	// function getOrCreateUser(callback) {
+    //     axios.put(
+    //         'https://api.chatengine.io/users/',
+    //         {username: [username]},
 
-		}
-	function renderChatForm(creds) {
-		return (
-			<div>
+    //         {headers: {"Private-Key": process.env.projectID}}
+    //     )
+    //     .then(r => callback(r.data))
+    //     .catch(e => console.log('Get or create user error', e))
+
+	// 	}
+	// function renderChatForm(creds) {
+	// 	return (
+	// 		<div>
 			
-			</div>
-		)
-	}
+	// 		</div>
+	// 	)
+	// }
 
 
 return(
@@ -48,9 +54,25 @@ return(
            </div>
       </div>
         <ChatEngine
+		userName=''
 		userSecret=''
-		projectID={process.env.projectID}
-		userName= {[username]}
+		projectID='b29c0382-07ab-44d3-a3e1-86606070fac5'
+		// userName= {[username]}
+		renderChatList={(chatAppState) => <ChatList {...chatAppState} />}
+		renderChatCard={(chat, index) => <ChatCard key={`${index}`} chat={chat} />}
+		renderNewChatForm={(creds) => <NewChatForm creds={creds} />} 
+		renderChatFeed={(chatAppState) => <ChatFeed {...chatAppState} />}
+		renderChatHeader={(chat) => <ChatHeader />}
+		renderIceBreaker={(chat) => <IceBreaker />}
+		renderMessageBubble={(creds, chat, lastMessage, message, nextMessage) => <MessageBubble lastMessage={lastMessage} message={message} nextMessage={nextMessage} chat={chat} />}
+		renderIsTyping={(typers) => <IsTyping />}
+		renderConnectionBar={(chat) => <ConnectionBar />}
+		renderNewMessageForm={(creds, chatID) => <NewMessageForm />}
+		renderChatSettings={(chatAppState) => <ChatSettings {...chatAppState} />}
+		renderChatSettingsTop={(creds, chat) => <ChatSettingsTop />}
+		renderPeopleSettings={(creds, chat) => <PeopleSettings />}
+		renderPhotosSettings={(chat) => <PhotosSettings />}
+		renderOptionsSettings={(creds, chat) => <OptionsSettings />}
         />
 		</div>
       )
